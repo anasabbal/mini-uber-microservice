a1e93a590f04c9e19bd5d15dffb25fd8f213f0e9
anas <anas.abbal66@gmail.com>
2023-01-02 03:03:12 +0100

    extends an example of circuit breaker(#01)

--- circuit-breaker/src/main/java/com/nas/circuit/config/CircuitBreakerConfiguration.java
+++ circuit-breaker/src/main/java/com/nas/circuit/config/CircuitBreakerConfiguration.java
@@ -1,19 +1,20 @@
 package com.nas.circuit.config;
 
+
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
-import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreakerFactory;
-import org.springframework.context.annotation.Bean;
 
 import java.time.Duration;
 
 public class CircuitBreakerConfiguration {
 
     @Bean
-    public Customizer<ReactiveCircuitBreakerFactory> defaultCustomizer(){
-        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder((String) id)
+    public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer(){
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
                 .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
                 .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build()).build());
     }

9189e17e167a677300f59821e945d0580c1c8055
anasabbal <anas.abbal66@gmail.com>
2023-01-30 15:22:14 +0100

    extends react (typescript) => customer-service

---
+++ customer-service/customer-ui/src/type/driver.ts
@@ -0,0 +1,8 @@
+export default interface DriverResponse{
+    id?: string,
+    createdBy?: string,
+    updatedBy?: string,
+    deleted?: boolean,
+    firstName?: string,
+    lastName?: string
+};

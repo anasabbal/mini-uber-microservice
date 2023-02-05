08b03f1a892601b1f4bec7b512450aa525d2a08f
anasabbal <anas.abbal66@gmail.com>
2023-02-05 13:53:13 +0100

    extends wallet-service microservice

--- config-server/src/main/java/com/nas/config/server/ConfigServerApplication.java
+++ config-server/src/main/java/com/nas/config/server/ConfigServerApplication.java
@@ -5,12 +5,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @EnableConfigServer
-@RefreshScope
 public class ConfigServerApplication{
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);

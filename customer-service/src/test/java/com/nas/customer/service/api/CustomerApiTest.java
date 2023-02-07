abea759661aaa0380aabc330c2f28a8ff6135399
anasabbal <anas.abbal66@gmail.com>
2023-02-07 14:01:19 +0100

    begin unit testing in customer-service

--- customer-service/src/test/java/com/nas/customer/service/api/CustomerApiTest.java
+++ customer-service/src/test/java/com/nas/customer/service/api/CustomerApiTest.java
@@ -1,4 +1,96 @@
 package com.nas.customer.service.api;
 
+
+import com.nas.customer.service.command.CustomerCommand;
+import com.nas.customer.service.controller.CustomerController;
+import com.nas.customer.service.dto.mapper.CustomerMapper;
+import com.nas.customer.service.model.Customer;
+import com.nas.customer.service.repository.CustomerRepository;
+import com.nas.customer.service.service.CustomerService;
+import com.nas.customer.service.service.CustomerServiceImpl;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(value = CustomerController.class)
 public class CustomerApiTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @InjectMocks
+    private CustomerService customerService;
+
+    @Mock
+    private List<Customer> customers;
+
+    @Mock
+    private RestTemplate restTemplate;
+
+    @Mock
+    private RabbitTemplate rabbitTemplate;
+
+    @Mock
+    private CustomerRepository customerRepository;
+
+    @BeforeClass
+    public void setUp() throws Exception {
+        //Initialize all mock objects
+        MockitoAnnotations.initMocks(this);
+        CustomerCommand customerCommand = new CustomerCommand();
+        customerCommand.setFirstName("anas");
+        customerCommand.setLastName("abbal");
+        customerCommand.setPassword("zadina123");
+        customerCommand.setEmail("anas.abbal10@gmail.com");
+        customers.add(Customer.create(customerCommand));
+        customerService = new CustomerServiceImpl(customerRepository, restTemplate, rabbitTemplate);
+    }
+
+    @Test
+    @DisplayName("Test Find All Customers")
+    public void find_all_customers() throws Exception {
+        PageRequest pageRequest = PageRequest.of(0, 10);
+        List<Customer> customersList = customers;
+        Page<Customer> pageCustomer = new PageImpl<>(customersList);
+
+        // when
+        when(customerService.findAllByDeletedFalse(pageRequest)).thenReturn(pageCustomer);
+
+        // then
+        mockMvc.perform(get("/v1/customers")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        // verify
+        verify(customerService, times(1)).findAllByDeletedFalse(pageRequest);
+    }
 }
--- customer-service/src/test/java/com/nas/customer/service/service/CustomerRepoTest.java
+++ customer-service/src/test/java/com/nas/customer/service/service/CustomerRepoTest.java
@@ -1,27 +1,72 @@
 package com.nas.customer.service.service;
 
+import com.nas.customer.service.command.CustomerCommand;
+import com.nas.customer.service.model.Customer;
 import com.nas.customer.service.repository.CustomerRepository;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
 public class CustomerRepoTest {
 
+
+    /**
+     * @InjectMocks, the phrase inject might be deceptive.
+     * This is a Mockito utility that relieves us of the responsibility of constructing an instance of the class under test.
+     * In a word, Mockito will look for a suitable public function Object() { [native code] } to create an instance of our
+     * CustomerServiceImpl and pass it all mocks (there is just one).
+     */
+    @InjectMocks
+    private CustomerServiceImpl customerService;
+
+    /**
+     * we have @Mock annotation on
+     * Client class because we need client class in our test but we do not want to invoke the original class.
+     * so, we 'Mock' that class which works the same as the original but is actually just a mock
+     *
+     */
+    @Mock
+    private Customer customer;
+
+    @Mock
+    private CustomerCommand customerCommand;
 
     @Mock
     private CustomerRepository customerRepository;
 
-    @BeforeAll
+    @BeforeClass
     public void setUp(){
         MockitoAnnotations.initMocks(this); // this is needed for init of mocks, if you use @Mock
-        
+        customerCommand = new CustomerCommand();
+        customerCommand.setFirstName("anas");
+        customerCommand.setLastName("abbal");
+        customerCommand.setPassword("zadina123");
+        customerCommand.setEmail("anas.abbal10@gmail.com");
+        customer = Customer.create(customerCommand);
     }
-
 
     @Test
     public void should_can_i_create_customer(){
+        //when(customerRepository.save(any(Customer.class))).thenReturn(customer);
+        //assertEquals("anas" , customer.getFirstName());
 
+        //verify(customerRepository, times(1)).save(any(Customer.class));
+        //assertEquals("anas.abbal10@gmail.com", customer1.getEmail());
     }
 
 
@@ -36,6 +81,16 @@ public class CustomerRepoTest {
     @Test
     public void should_can_i_update_without_validate(){
 
+    }
+    @Test
+    public void should_i_find_all_customers(){
+        final CustomerCommand customerCommand = new CustomerCommand();
+        customerCommand.setEmail("anas.abbal10@gmail.com");
+        when(customerRepository.findAll()).
+                thenReturn(Arrays.asList(Customer.create(customerCommand)));
+        List<Customer> customers = customerService.findAll();
+
+        assertEquals("anas.abbal10@gmail.com", customers.get(0).getEmail());
     }
     @Test
     public void should_can_i_update_with_wrong_payload(){

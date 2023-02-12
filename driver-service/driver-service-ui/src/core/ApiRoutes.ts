256721edc8b1797678883eda8adb167ee84b5995
anasabbal <anas.abbal66@gmail.com>
2023-02-12 15:43:43 +0100

    remove front

--- driver-service/driver-service-ui/src/core/ApiRoutes.ts
+++
@@ -1,5 +0,0 @@
-const ApiRoutes = Object.freeze({
-    about: '/v1/drivers'
-});
-
-export default ApiRoutes;
--- driver-service/driver-service-ui/src/core/http-common.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: "http://localhost:8081/",
-    headers: {
-        "Content-type": "application/json"
-    }
-});

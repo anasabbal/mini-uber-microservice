945ee043f79ba1a519338d94a54ea55dac975653
anasabbal <anas.abbal66@gmail.com>
2023-02-15 16:12:27 +0100

    FEAT:[NUS-01]: extends project by updating api gateway

--- driver-service/src/main/java/com/nas/driver/dto/mapper/NotificationDriverMapper.java
+++ driver-service/src/main/java/com/nas/driver/dto/mapper/NotificationDriverMapper.java
@@ -4,7 +4,6 @@ package com.nas.driver.dto.mapper;
 import com.nas.driver.dto.NotificationDriverDto;
 import com.nas.driver.model.NotificationDriver;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public abstract class NotificationDriverMapper {

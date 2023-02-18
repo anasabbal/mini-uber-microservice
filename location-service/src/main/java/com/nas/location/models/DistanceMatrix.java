2039318e3db2cdf77f151dfbf00bd4680f46a24b
anasabbal <anas.abbal66@gmail.com>
2023-02-18 14:54:16 +0100

    begin location-service complex microservice

--- location-service/src/main/java/com/nas/location/models/DistanceMatrix.java
+++ location-service/src/main/java/com/nas/location/models/DistanceMatrix.java
@@ -1,7 +1,17 @@
 package com.nas.location.models;
 
 public class DistanceMatrix {
-    private String[] originAddress;
-    private String[] destinationAddress;
+    private Address originAddress;
+    private Address destinationAddress;
     private DistanceMatrixRow[] rows;
+
+    public static DistanceMatrix create(Address originAddress, Address destinationAddress, DistanceMatrixRow[] rows) {
+        final DistanceMatrix distanceMatrix = new DistanceMatrix();
+
+        distanceMatrix.originAddress = originAddress;
+        distanceMatrix.destinationAddress = destinationAddress;
+        distanceMatrix.rows = rows;
+
+        return distanceMatrix;
+    }
 }

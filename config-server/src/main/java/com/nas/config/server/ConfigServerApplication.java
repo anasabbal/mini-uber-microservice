abbfd7c360ba03abab7be0b3d0e8073bf8a583e1
Anas Abbal <87352192+anasabbal@users.noreply.github.com>
2023-02-27 21:05:28 +0100

    begin updating to release 0.0.3 => add generic RestTemplate in config server

--- config-server/src/main/java/com/nas/config/server/ConfigServerApplication.java
+++ config-server/src/main/java/com/nas/config/server/ConfigServerApplication.java
@@ -1,26 +1,14 @@
 package com.nas.config.server;
 
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication{
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
-    }
-
-
-    @Value("${server.url:Unable to connect to config server}")
-    private String uri;
-
-
-    @GetMapping("/server/url")
-    public String get(){
-        return this.uri;
     }
 }

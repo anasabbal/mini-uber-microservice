8b5318e14e9883d14a32724d970f3003a53988c2
Anas Abbal <87352192+anasabbal@users.noreply.github.com>
2023-02-27 21:05:28 +0100

    begin updating to release 0.0.3 => add generic RestTemplate in config server

---
+++ config-server/src/main/java/com/nas/config/server/config/JavaConfig.java
@@ -0,0 +1,18 @@
+package com.nas.config.server.config;
+
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class JavaConfig {
+
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}

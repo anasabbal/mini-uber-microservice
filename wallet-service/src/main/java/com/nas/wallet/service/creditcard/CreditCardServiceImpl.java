57d97b0fc37ea190f394a151a8dd758b1b97ea5c
Anas Abbal <87352192+anasabbal@users.noreply.github.com>
2023-03-03 18:43:10 +0100

    begin extending payment-service # 00003

--- wallet-service/src/main/java/com/nas/wallet/service/creditcard/CreditCardServiceImpl.java
+++ wallet-service/src/main/java/com/nas/wallet/service/creditcard/CreditCardServiceImpl.java
@@ -9,7 +9,7 @@ import com.nas.wallet.command.CreditCardCommand;
 import com.nas.wallet.model.CreditCard;
 import com.nas.wallet.model.Wallet;
 import com.nas.wallet.repository.CreditCardRepository;
-import com.nas.wallet.repository.WalletRepository;
+import com.nas.wallet.service.wallet.WalletService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -22,22 +22,25 @@ import org.springframework.transaction.annotation.Transactional;
 public class CreditCardServiceImpl implements CreditCardService{
 
     private final CreditCardRepository creditCardRepository;
-    private final WalletRepository walletRepository;
+    private final WalletService walletService;
 
 
     @Override
     public CreditCard create(String walletId, CreditCardCommand creditCardCommand) {
-        log.info("[+] Begin fetching wallet with account id {}", walletId);
-        final Wallet wallet = walletRepository.findWalletByAccountId(walletId).orElseThrow(
-                () -> new BusinessException(ExceptionPayloadFactory.WALLER_FOR_ACCOUNT_NOT_FOUND.get())
-        );
-        log.info("[+] Wallet with id {} fetched successfully", wallet.getId());
+        final Wallet wallet = walletService.findById(walletId);
 
         log.info("[+] Begin creating Credit Card with payload {}", JSONUtil.toJSON(creditCardCommand));
         final CreditCard creditCard = CreditCard.create(creditCardCommand);
+
         creditCard.linkToWallet(wallet);
         wallet.addCreditCard(creditCard);
-        log.info("[+] Credit Card with id {} created successfully", creditCard.getId());
+
         return creditCardRepository.save(creditCard);
+    }
+    @Override
+    public CreditCard findById(String creditCardId){
+        return creditCardRepository.findById(creditCardId).orElseThrow(
+                () -> new BusinessException(ExceptionPayloadFactory.CREDIT_CARD_NOT_FOUND.get())
+        );
     }
 }

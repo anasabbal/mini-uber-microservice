d5a9643c8b511c13dd93271e2d6b5332fb0e844a
Anas Abbal <87352192+anasabbal@users.noreply.github.com>
2023-03-08 18:08:25 +0100

    FEAT:[NAS-18] => add criteria

--- customer-service/src/main/java/com/nas/customer/service/repository/CustomerRepository.java
+++ customer-service/src/main/java/com/nas/customer/service/repository/CustomerRepository.java
@@ -1,15 +1,36 @@
 package com.nas.customer.service.repository;
 
+import com.nas.core.util.JSONUtil;
+import com.nas.core.util.PatternUtil;
+import com.nas.customer.service.criteria.CustomerCriteria;
 import com.nas.customer.service.model.Customer;
+import static com.nas.customer.service.model.Customer_.*;
+import jakarta.persistence.criteria.Predicate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, String> , JpaSpecificationExecutor<Customer> {
+    public final static Logger log = LoggerFactory.getLogger(CustomerRepository.class);
     Page<Customer> findCustomersByDeletedFalse(Pageable pageable);
     Customer findByDriverId(String driverId);
+
+    default Page<Customer> findAllByCriteria(Pageable pageable, CustomerCriteria customerCriteria){
+        return findAll((root , query, builder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+            if (customerCriteria.firstName() != null) {
+                String pattern = PatternUtil.likePattern(customerCriteria.firstName().toUpperCase());
+                predicates.add(builder.like(builder.upper(root.get(FIRST_NAME)), pattern));
+            }
+            return builder.and(predicates.toArray(new Predicate[]{}));
+        }, pageable);
+    }
 }
--- driver-service/src/main/java/com/nas/driver/criteria/DriverCriteria.java
+++ driver-service/src/main/java/com/nas/driver/criteria/DriverCriteria.java
@@ -3,12 +3,19 @@ package com.nas.driver.criteria;
 
 import com.nas.driver.model.DriverStatus;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-public class DriverCriteria {
-    private String firstName;
-    private String lastName;
-    private DriverStatus status;
+
+public record DriverCriteria(String firstName, DriverStatus status) {
+
+    @Override
+    public String firstName() {
+        return firstName;
+    }
+
+    @Override
+    public DriverStatus status() {
+        return status;
+    }
 }

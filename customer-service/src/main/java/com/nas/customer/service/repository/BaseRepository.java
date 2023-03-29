4a1052cf347aca1cfa14efa0d5a31556ac0ba33a
Anas Abbal <87352192+anasabbal@users.noreply.github.com>
2023-03-29 00:28:03 +0000

    FEAT:[NAS-26] => switch to GENERIC

--- customer-service/src/main/java/com/nas/customer/service/repository/BaseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nas.customer.service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-
-@Repository
-public interface BaseRepository<T> extends JpaRepository<T, String> {
-}

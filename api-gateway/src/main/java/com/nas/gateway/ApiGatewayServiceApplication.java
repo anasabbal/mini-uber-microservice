c54eafd8dbcbc3ac785f171dcdbc97c18ae213b9
anasabbal <anas.abbal66@gmail.com>
2024-04-14 15:49:14 +0100

    FEAT:[refactor-v03]=> Refactor

--- api-gateway/src/main/java/com/nas/gateway/ApiGatewayServiceApplication.java
+++ api-gateway/src/main/java/com/nas/gateway/ApiGatewayServiceApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableWebFlux
 @EnableDiscoveryClient
 public class ApiGatewayServiceApplication {
     public static void main(String[] args) {
---
+++ api-gateway/src/main/java/com/nas/gateway/config/AuthenticationFilter.java
@@ -0,0 +1,73 @@
+package com.nas.gateway.config;
+
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@RefreshScope
+@RequiredArgsConstructor
+public class AuthenticationFilter implements GatewayFilter {
+
+    private final RouterValidator routerValidator;
+    private final TokenHandler tokenHandler;
+
+
+    @Value("${token.signing.key}")
+    private String tokenPrefix;
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+
+        if (routerValidator.isSecured.test(request)) {
+            if (this.isAuthMissing(request) || this.isPrefixMissing(request))
+                return this.onError(exchange, "Authorization header is missing in request", HttpStatus.UNAUTHORIZED);
+
+            final String token = this.getAuthHeader(request);
+
+            if (tokenHandler.isInvalid(token))
+                return this.onError(exchange, "Authorization header is invalid", HttpStatus.UNAUTHORIZED);
+
+            this.populateRequestWithHeaders(exchange, token);
+        }
+        return chain.filter(exchange);
+    }
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+        return response.setComplete();
+    }
+
+    private String getAuthHeader(ServerHttpRequest request) {
+        var header = request.getHeaders().getOrEmpty("Authorization").get(0);
+        return header.replace(tokenPrefix,"").trim();
+    }
+
+    private boolean isAuthMissing(ServerHttpRequest request) {
+        return !request.getHeaders().containsKey("Authorization");
+    }
+
+    private boolean isPrefixMissing(ServerHttpRequest request) {
+        var header = request.getHeaders().getFirst ("Authorization");
+        assert header != null;
+        return !header.startsWith(tokenPrefix);
+    }
+
+    private void populateRequestWithHeaders(ServerWebExchange exchange, String token) {
+        Claims claims = tokenHandler.getAllClaimsFromToken(token);
+        exchange.getRequest().mutate()
+                .header("id", String.valueOf(claims.get("id")))
+                .header("roles", String.valueOf(claims.get("roles")))
+                .header("tenantId", String.valueOf(claims.get("tenantId")))
+                .build();
+    }
+}
